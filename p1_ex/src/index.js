caceed0589b860792aa02ef7e0e606b50f232ad2
Louis <louisglhuynh@gmail.com>
2020-04-18 16:58:50 -0400

    ex 1.3 v2

--- p1_ex/src/index.js
+++ p1_ex/src/index.js
@@ -5,20 +5,11 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-// const Content = (props) => {
-//   console.log(props);
-//   return (
-//     <p>
-//       {props.content.name} {props.content.exercises}
-//     </p>
-//   );
-// };
-
 const Content = (props) => {
   console.log(props);
   return (
     <p>
-      {props.name} {props.exercises}
+      {props.content.name} {props.content.exercises}
     </p>
   );
 };
@@ -46,12 +37,9 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content name={part1.name} exercises={part1.exercises} />
-      <Content name={part2.name} exercises={part2.exercises} />
-      <Content name={part3.name} exercises={part3.exercises} />
-      {/* <Content content={part1} />
+      <Content content={part1} />
       <Content content={part2} />
-      <Content content={part3} /> */}
+      <Content content={part3} />
       <Total
         ex1={part1.exercises}
         ex2={part2.exercises}
